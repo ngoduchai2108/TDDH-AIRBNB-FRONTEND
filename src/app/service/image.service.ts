f3868cbfded893b5cfbd4ce7e264e5a8a3157f93
dinh <dinh.kttn.neu@gmail.com>
2019-06-27 17:25:33 +0700

    update edit houseComponent

--- src/app/service/image.service.ts
+++ src/app/service/image.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 
@@ -6,24 +6,27 @@ import {Observable} from 'rxjs';
   providedIn: 'root'
 })
 export class ImageService {
-  private readonly API_URL =  'http://localhost:8080/api/auth';
+  private readonly API_URL = 'http://localhost:8080/api/auth';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
 
   // getImages(): Observable<any> {
   //   // @ts-ignore
   //   return this.http.get<any>(this.API_URL + 'houses');
   // }
-  // getImage(): Observable<any> {
-  //   return this.http.get<any>( this.API_URL + 'house');
-  // }
-  //
+  getImage(id: number): Observable<any> {
+    return this.http.get<any>(this.API_URL + '/download-file/' + id);
+  }
+
   remove(id: number): Observable<any> {
     return this.http.delete<number>(this.API_URL + '/house/' + id);
   }
+
   removeAllByHouseID(id: number): Observable<any> {
     return this.http.delete<number>(this.API_URL + '/delete-all-files/' + id);
   }
+
   //
   // update(house: any): Observable<any> {
   //   return this.http.put<any>(this.API_URL + 'house/' + house.id, house);
@@ -31,6 +34,10 @@ export class ImageService {
 
   create(houseid: any, img: any): Observable<any> {
     return this.http.post<any>(this.API_URL + '/upload-file/' + houseid, img);
+  }
+
+  getListIdByHouseId(id: number) {
+    return this.http.get<any>(this.API_URL + '/download-all-file/'+id);
   }
 }
 
--- src/app/user/edit-house/edit-house.component.ts
+++ src/app/user/edit-house/edit-house.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {ImageService} from "../../service/image.service";
+import {CategoriesHouse} from "../../model/CategoriesHouse";
+import {HouseService} from "../../service/house-service.service";
+import {CategorieshouseService} from "../../service/categorieshouse.service";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-edit-house',
@@ -7,9 +13,82 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditHouseComponent implements OnInit {
 
-  constructor() { }
+  imageToShow: any;
+  formHouseData: FormGroup;
+  editFail = false;
+  listCateGories: CategoriesHouse[];
+  currentHouse ={
+    categories: {name:''}
+  };
+  listCurrentImageId = [];
 
-  ngOnInit() {
+  constructor(private  imageService: ImageService,
+              private houseService: HouseService,
+              private cateService: CategorieshouseService,
+              private route: ActivatedRoute,
+              private fb: FormBuilder,
+              private router: Router) {
   }
 
+  ngOnInit() {
+    this.formHouseData = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      categories: ['', [Validators.required]],
+      address: ['', [Validators.required, Validators.minLength(3)]],
+      quantityBathroom: ['', [Validators.required, Validators.min(1)]],
+      quantityBedroom: ['', [Validators.required, Validators.min(1)]],
+      description: ['', [Validators.required, Validators.minLength(10)]],
+      price: ['', [Validators.required, Validators.min(0)]],
+    });
+    // this.getImageFromService();
+    const id = +this.route.snapshot.paramMap.get('id');
+
+    this.houseService.getHouse(id).subscribe(next => {
+      this.currentHouse = next;
+      console.log(next);
+      this.formHouseData.patchValue(this.currentHouse)
+    }, error => console.log(error));
+    this.cateService.getCategories().subscribe(next => {
+      this.listCateGories = next;
+    })
+    // this.imageService.getListIdByHouseId(id).subscribe(next => this.listCurrentImageId = next);
+
+  }
+
+  createImageFromBlob(image: Blob) {
+    const reader = new FileReader();
+    reader.addEventListener('load', () => {
+      this.imageToShow = reader.result;
+    }, false);
+    if (image) {
+      reader.readAsDataURL(image);
+    }
+  }
+
+  getImageFromService() {
+    this.imageService.getImage(72).subscribe(data => {
+      this.createImageFromBlob(data);
+      console.log(data);
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  editHouse() {
+    if (this.formHouseData.valid){
+      const {value} = this.formHouseData;
+      const data = {
+        ...this.currentHouse,
+        ...value
+      };
+      console.log(data);
+      this.houseService.update(data).subscribe(next => {
+        this.router.navigate(['/houses']),
+          error => {
+          console.log(error);
+          }
+      });
+    }
+  }
 }
+
